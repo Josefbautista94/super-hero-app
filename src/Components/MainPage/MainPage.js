e53ce2b6519662b67891e7d820af283569e06d23
Jose <josefbautista94@gmail.com>
2023-09-27 17:21:46 -0400

    connected to an Open API that has info on Superheros/Villans

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,18 +1,52 @@
-import React, {Component} from 'react';
-import "./MainPage.css"
+import React, { Component } from 'react';
+import "./MainPage.css";
 import axios from 'axios';
 
-
 class MainPage extends Component {
+    // 1. Initialize state
+    state = {
+        superheroes: [],
+        loading: true,
+        error: null
+    };
 
-render() {
+    // 2. Fetch data in componentDidMount
+    componentDidMount() {
+        const API_ENDPOINT = process.env.REACT_APP_SUPERHERO_API_ENDPOINT;
     
-    return (
-<div> Hi</div>
+        axios.get(API_ENDPOINT)
+            .then(response => {
+                this.setState({
+                    superheroes: response.data,
+                    loading: false
+                });
+            })
+            .catch(error => {
+                console.error("There was an error fetching data:", error);
+                this.setState({
+                    error: error.message,
+                    loading: false
+                });
+            });
+    }
+    
+    render() {
+        const { superheroes, loading, error } = this.state;
 
-)
+        if (loading) return <div>Loading...</div>;
+        if (error) return <div>Error: {error}</div>;
 
-}
+        return (
+            <div>
+                <h1>Superheroes</h1>
+                <ul>
+                    {superheroes.map(hero => (
+                        <li key={hero.id}>{hero.name}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default MainPage;
