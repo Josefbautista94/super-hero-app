07c5fba4d1d101ffe1d28e419ab67ae695767179
Jose <josefbautista94@gmail.com>
2023-09-27 16:08:04 -0400

    installed react-router and Axios to my project

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import "./MainPage.css"
+import axios from 'axios';
+
+
+class MainPage extends Component {
+
+render() {
+    
+    return (
+<div> Hi</div>
+
+)
+
+}
+}
+
+export default MainPage;
