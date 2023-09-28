795c5b3a70f4e46e5e0f1deb901ced0e97d04717
Jose <josefbautista94@gmail.com>
2023-09-27 23:04:26 -0400

    Changed the tab icon in the web browser

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -38,23 +38,29 @@ class MainPage extends Component {
 
   handleSearchChange = (event) => {
     this.setState({ searchTerm: event.target.value });
-}
+  };
 
   render() {
-    const { superheroes, loading, error, filter, searchTerm  } = this.state;
-    
-    const filteredHeroes = superheroes.filter(hero => {
-        // Filter by alignment
-        if (filter !== 'all' && hero.biography.alignment.toLowerCase() !== filter) {
-            return false;
-        }
-    
-        // Filter by search term
-        if (searchTerm && !hero.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return false;
-        }
-    
-        return true;
+    const { superheroes, loading, error, filter, searchTerm } = this.state;
+
+    const filteredHeroes = superheroes.filter((hero) => {
+      // Filter by alignment
+      if (
+        filter !== "all" &&
+        hero.biography.alignment.toLowerCase() !== filter
+      ) {
+        return false;
+      }
+
+      // Filter by search term
+      if (
+        searchTerm &&
+        !hero.name.toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        return false;
+      }
+
+      return true;
     });
 
     if (loading) return <div>Loading...</div>;
@@ -63,13 +69,13 @@ class MainPage extends Component {
     return (
       <div>
         <h1 className="superHeroTitle">Superheroes</h1>
-
-        <input 
-    type="text" 
-    placeholder="Search for a superhero..." 
-    value={this.state.searchTerm} 
-    onChange={this.handleSearchChange}
-/>        <div className="filter-menu">
+        <input
+          type="text"
+          placeholder="Search for a superhero..."
+          value={this.state.searchTerm}
+          onChange={this.handleSearchChange}
+        />{" "}
+        <div className="filter-menu">
           <label>Filter by Alignment: </label>
           <select value={filter} onChange={this.handleFilterChange}>
             <option value="all">See All</option>
@@ -78,17 +84,27 @@ class MainPage extends Component {
             <option value="neutral">Neutral</option>
           </select>
         </div>
-
         <ul className="hero-grid">
           {filteredHeroes.map((hero) => (
             <li key={hero.id} className="hero-li">
               <div className="hero-name">{hero.name}</div>
               <img src={hero.images.md} alt={hero.name} />
               <div className="hero-details">
-              <div>Full Name: {hero.biography.fullName ? hero.biography.fullName : hero.name}</div>
-              <div>Gender: {hero.appearance.gender}</div>
-                <div>Height: {hero.appearance.height[0]}, {hero.appearance.height[1]}</div>          
-                <div>Weight: {hero.appearance.weight[0]}, {hero.appearance.weight[1]}</div>
+                <div>
+                  Full Name:{" "}
+                  {hero.biography.fullName
+                    ? hero.biography.fullName
+                    : hero.name}
+                </div>
+                <div>Gender: {hero.appearance.gender}</div>
+                <div>
+                  Height: {hero.appearance.height[0]},{" "}
+                  {hero.appearance.height[1]}
+                </div>
+                <div>
+                  Weight: {hero.appearance.weight[0]},{" "}
+                  {hero.appearance.weight[1]}
+                </div>
                 <div>Power Stats: </div>
                 <div>Intelligence: {hero.powerstats.intelligence}</div>
                 <div>Strength: {hero.powerstats.strength}</div>
@@ -96,11 +112,20 @@ class MainPage extends Component {
                 <div>Durability: {hero.powerstats.durability}</div>
                 <div>Power: {hero.powerstats.power}</div>
                 <div>Combat: {hero.powerstats.combat}</div>
-                <div>Place Of Birth: {hero.biography.placeOfBirth !== '-' ? hero.biography.placeOfBirth : "Not Known"}</div>
+                <div>
+                  Place Of Birth:{" "}
+                  {hero.biography.placeOfBirth !== "-"
+                    ? hero.biography.placeOfBirth
+                    : "Not Known"}
+                </div>
                 <div>Race: {hero.appearance.race}</div>
                 <div>Alignment: {hero.biography.alignment}</div>
-                <div>Occupation: {hero.work.occupation !== '-' ? hero.work.occupation : 'Not known'}</div>
-               
+                <div>
+                  Occupation:{" "}
+                  {hero.work.occupation !== "-"
+                    ? hero.work.occupation
+                    : "Not known"}
+                </div>
               </div>
             </li>
           ))}
